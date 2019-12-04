efa174a0ff8bf05fb45650dad8de6fa67fd58489
tofique <tofiques93@gmail.com>
2019-12-03 21:03:14 -0500

    part one refactored

--- day1/fuel-calc.py
+++ day1/fuel-calc.py
@@ -4,6 +4,6 @@ with open(filename) as f:
     lines = f.read().splitlines()
     lines = list(map(int, lines))
 
-result = map(lambda x: ((x // 3) - 2), lines)
-total_mass = sum(result)
+lines = map(lambda x: ((x // 3) - 2), lines)
+total_mass = sum(lines)
 print(total_mass)
